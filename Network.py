559bb060d55c899ef7fdbe2443db59c8d8d57504
Guilherme Zilli <guilherme_zilli@hotmail.com>
2022-12-14 23:01:22 -0500

    Initial Commit

--- Network.py
+++ Network.py
@@ -14,17 +14,58 @@ class BaseStation():
         self.height = height
         self.tx_power_dB = tx_power_dB
 
+    def grid_signal_power(self, grid):
+        signal_power = np.zeros([grid.Nx,grid.Ny])
+        for idx_x in range(grid.Nx):
+            for idx_y in range(grid.Ny):
+                dist = ((grid.coord_x[idx_x,idx_y] - self.pos_x)**2 + (grid.coord_y[idx_x,idx_y] - self.pos_y)**2 )**0.5
+                if dist == 0:
+                    sigpower = self.tx_power_dB
+                else:
+                    sigpower = 10*np.log10(10**(self.tx_power_dB/10) * 1/(dist**2))
+                print(f'x:{idx_x}, y:{idx_x}, sig:{sigpower}')
+                signal_power[idx_x][idx_y] = sigpower
 
-    def grid_signal_power(self, grid:int):
-        pass
+        return signal_power
 
 class Grid():
-    def __init__(self, x_length: float = 100, y_length: float = 100, Nx: int = 100, Ny: int = 100 ):
-        coord_x, coord_y = np.meshgrid(np.arange(Nx), np.arange(Nx), sparse=False, indexing='xy')
-        coord_x, coord_y = x_length*coord_x, y_length*coord_y
+    def __init__(self, x_length: float = 100, y_length: float = 100, Nx: int = 100, Ny: int = 100,
+                 center_at_origin: bool = True):
+        self.Nx = Nx
+        self.Ny = Ny
+        self.coord_x, self.coord_y = np.meshgrid(np.arange(Nx+1), np.arange(Ny+1), sparse=False, indexing='xy')
+        self.coord_x, self.coord_y = x_length*self.coord_x/Nx, y_length*self.coord_y/Ny
+        # self.coord_x = self.coord_x.reshape(-1)
+        # self.coord_y = self.coord_y.reshape(-1)
+        # self.coord_x = self.coord_x.flatten()
+        # self.coord_y = self.coord_y.flatten()
+
+        if center_at_origin:
+            self.coord_x = self.coord_x - x_length/2
+            self.coord_y = self.coord_y - x_length/2
+
+        # self.coordinates = [self.coord_x, self.coord_y]
+    # TODO: Plotable grid (draw lines)
 
 
 if __name__ == "__main__":
     # Main function includes multiple examples
+    grid = Grid()
+    # print(grid.coord_x.shape, grid.coord_y.shape)
+    BS1 = BaseStation()
+    BS2 = BaseStation(pos_x=20, pos_y=20)
+    signal_power_1 = BS1.grid_signal_power(grid)
+    signal_power_2 = BS2.grid_signal_power(grid)
 
-    BS = BaseStation()
+    print((grid.coord_y))
+    # print(shape(signal_power_2))
+    #
+    # print(len(signal_power_1))
+    # print(len(signal_power_2))
+
+    signal_power = signal_power_1 + signal_power_2
+
+    # print(signal_power)
+    fig, ax = plt.subplots()
+    ax.pcolormesh(grid.coord_x, grid.coord_y, signal_power, shading='auto')
+    plt.show()
