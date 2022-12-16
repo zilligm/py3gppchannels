80eaf97b0dfc40660cbba5d0e167c90c22878733
Guilherme Zilli <guilherme_zilli@hotmail.com>
2022-12-15 22:37:22 -0500

    Added Class UserEquipment; can place and plot UEs in the map and attach UE to BS.

--- Network.py
+++ Network.py
@@ -2,12 +2,15 @@ import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
-
+import itertools
 import hexalattice
 
 
-class BaseStation():
+class BaseStation:
+    id_iter = itertools.count()
+
     def __init__(self, pos_x: float = 0, pos_y: float = 0, height: float = 10, tx_power_dB: float = 20):
+        self.ID = next(BaseStation.id_iter)
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.height = height
@@ -21,12 +24,41 @@ class BaseStation():
                 if dist == 0:
                     sigpower = self.tx_power_dB
                 else:
-                    sigpower = 10*np.log10(10**(self.tx_power_dB/10) * 1/(dist**2))
+                    sigpower = 10 * np.log10(10 ** (self.tx_power_dB / 10) * 1 / (dist ** 2))
                 signal_power[idx_x][idx_y] = sigpower
 
         return signal_power
 
-class Grid():
+
+class UserEquipment:
+    id_iter = itertools.count()
+
+    def __init__(self, pos_x: float = 0, pos_y: float = 0, height: float = 1.5, tx_power_dB: float = 20):
+        self.ID = next(BaseStation.id_iter)
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.height = height
+        self.tx_power_dB = tx_power_dB
+        self.serving_cell = None
+        self.neighbor_cells = []
+
+    def attach(self, base_stations: list[BaseStation]):
+        rsrp = float('-inf')
+        for bs in base_stations:
+            dist = ((bs.pos_x - self.pos_x)**2 + (bs.pos_y - self.pos_y)**2)**0.5
+            if dist == 0:
+                rsrp_bs = bs.tx_power_dB
+            else:
+                rsrp_bs = 10 * np.log10(10 ** (bs.tx_power_dB / 10) * 1 / (dist ** 2))
+
+            if rsrp_bs > rsrp:
+                rsrp = rsrp_bs
+                self.serving_cell = bs.ID
+
+    # TODO: Handover method: select new cell, detach from current cell, attach to new cell
+
+
+class Grid:
     def __init__(self, x_length: float = 100, y_length: float = 100, Nx: int = 100, Ny: int = 100,
                  center_at_origin: bool = True):
         self.Nx = Nx
@@ -44,8 +76,8 @@ class Grid():
 
 if __name__ == "__main__":
     plt.ion()
-    Nx = 400
-    Ny = 400
+    Nx = 100
+    Ny = 100
 
     N = 3
     d = 50
@@ -55,7 +87,11 @@ if __name__ == "__main__":
     x_lim = h_ax.get_xlim()
     y_lim = h_ax.get_ylim()
 
-    grid = Grid(x_length=x_lim[1] - x_lim[0],y_length=y_lim[1] - y_lim[0], Nx=Nx, Ny=Ny)
+    UE_x_lim = (min(tile_centers_x) - d / 2, max(tile_centers_x) + d / 2)
+    UE_y_lim = (min(tile_centers_y) - d / 2, max(tile_centers_y) + d / 2)
+    print(UE_x_lim)
+
+    grid = Grid(x_length=x_lim[1] - x_lim[0], y_length=y_lim[1] - y_lim[0], Nx=Nx, Ny=Ny)
 
     NBS = len(tile_centers_x)
     BSs = []
@@ -70,16 +106,32 @@ if __name__ == "__main__":
     sinr = np.zeros([Nx, Ny, NBS])
     noise_floor = -120
     for i in range(NBS):
-        signal = 10**(signal_power[:, :, i]/10)
-        interference = np.ones([Nx, Ny]) * 10**(noise_floor/10)
-        for j in (np.delete(np.arange(NBS),i)):
-            interference = interference + 10**(signal_power[:, :, j]/10)
-        sinr[:, :, i] = 10*np.log10(signal / interference)
-        sinr[:, :, i] = np.clip(sinr[:, :, i], -40, 30)
+        signal = 10 ** (signal_power[:, :, i] / 10)
+        interference = np.ones([Nx, Ny]) * 10 ** (noise_floor / 10)
+        for j in (np.delete(np.arange(NBS), i)):
+            interference = interference + 10 ** (signal_power[:, :, j] / 10)
+        sinr[:, :, i] = 10 * np.log10(signal / interference)
+        sinr[:, :, i] = np.clip(sinr[:, :, i], -25, 25)
+
+    chart = h_ax.pcolormesh(grid.coord_x, grid.coord_y, sinr[:, :, 3], shading='auto', alpha=.8, cmap='turbo')
+
+    color = ['blue', 'orange', 'green', 'red', 'purple', 'brown', 'pink']
+    NUE = 400
+    UEs = []
+    for i in range(NUE):
+        pos_x = np.random.uniform(low=UE_x_lim[0], high=UE_x_lim[1], size=None)
+        pos_y = np.random.uniform(low=UE_y_lim[0], high=UE_y_lim[1], size=None)
+        UE = UserEquipment(pos_x=pos_x, pos_y=pos_y, tx_power_dB=18)
+        UEs.append(UE)
+        UE.attach(BSs)
+        # print(UE.serving_cell)
+        h_ax.scatter(UE.pos_x, UE.pos_y, color=color[UE.serving_cell], marker='.')
 
 
-    chart = h_ax.pcolormesh(grid.coord_x, grid.coord_y, total_signal_power, shading='auto', alpha=.8, cmap='turbo')
-    # chart = h_ax.pcolormesh(grid.coord_x, grid.coord_y, sinr[:, :, 3], shading='auto', alpha=.8, cmap='turbo')
+
+
+    # chart = h_ax.pcolormesh(grid.coord_x, grid.coord_y, total_signal_power, shading='auto', alpha=.8, cmap='turbo')
+
     plt.colorbar(chart, ax=h_ax)
 
     plt.show(block=True)
