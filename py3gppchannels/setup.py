c1b188d892477ffae0fcc7ca13cc315ef6cd42cc
Guilherme Zilli <guilherme_zilli@hotmail.com>
2023-05-22 17:19:45 -0400

    Refactored files, add more documentation

--- py3gppchannels/setup.py
+++ py3gppchannels/setup.py
@@ -4,12 +4,13 @@ VERSION = '0.0.1'
 DESCRIPTION = 'The py3gppchannel library is a Python package that implements the 3GPP standardized radio channel ' \
               'models (3GPP TR 38.901). It provides a comprehensive set of functions for simulating wireless ' \
               'communication systems and evaluating their performance under realistic channel conditions.'
+
 setup(
     author="Guilherme Zilli",
     description=DESCRIPTION,
     name="py3gppchannels",
     version=VERSION,
-    long_description= file: README.md,
+    url=https://github.com/zilligm/py3gppchannels,
     packages=find_packages(include=["matplotlib", "numpy", "itertools", "hexalattice"]),
     keywords=['wireless', 'wireless communication', 'wireless network', '3gpp', '5G', 'channel model', 'pathloss'],
     setup_requires=['wheel']
